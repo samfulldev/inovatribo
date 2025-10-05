34445d1aca66db79fd5ca02fba38ea61c8b06b3d
samfulldev <samuelcunhabr@gmail.com>
2025-10-05 14:20:18 -0300

    Atualizando server.js para usar process.env.PORT

--- server.js
+++ server.js
@@ -43,5 +43,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 
